f1853b0d7a6ee59b5c017542c9fc5edf640aa020
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2022-11-30 15:40:26 +0100

    add section cities

--- Side_page_template/app.js
+++ Side_page_template/app.js
@@ -1,3 +1,5 @@
+/* ##### FOOD ##### */
+
 let firstmenu = document.getElementById('firstmenu');
 let secondmenu = document.getElementById('secondmenu');
 let thirdmenu = document.getElementById('thirdmenu');
@@ -8,3 +10,43 @@ window.addEventListener('scroll', function(){
     secondmenu.style.bottom = value * 0.07 + 'px';
     thirdmenu.style.top =  value * 0.08 + 'px';
 })
+
+/* ##### END FOOD ##### */
+
+/* ##### Cities ##### */
+
+const content = document.querySelectorAll('.blank-country');
+const tab = document.querySelectorAll('.blank-link');
+const slider = document.querySelector('.blank-countries-slider');
+
+function hideTabContent() {
+  content.forEach(item => {
+    item.style.display = 'none';
+  });
+  tab.forEach(item => {
+    item.classList.remove('blank-active');
+  });
+}
+
+function showTabContent(i = 0) {
+  content[i].style.display = 'grid';
+  tab[i].classList.add('blank-active');
+}
+
+
+hideTabContent();
+showTabContent();
+
+slider.addEventListener("click", (e) => {
+  const target = e.target
+  if (target) {
+     tab.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+  }
+})
+
+/* ##### END Cities ##### */
