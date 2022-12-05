5f02107343d116c94d472109db9e8870557a5dad
Fensoa <hugo.andriamaromanana@laplateforme.io>
2022-12-05 10:10:19 +0100

    Changed blank to fit index

--- Side_page_Usa/app.js
+++ Side_page_Usa/app.js
@@ -15,22 +15,22 @@ window.addEventListener('scroll', function(){
 
 /* ##### Cities ##### */
 
-const content = document.querySelectorAll('.blank-country');
-const tab = document.querySelectorAll('.blank-link');
-const slider = document.querySelector('.blank-countries-slider');
+const content = document.querySelectorAll('.usa-country');
+const tab = document.querySelectorAll('.usa-link');
+const slider = document.querySelector('.usa-countries-slider');
 
 function hideTabContent() {
   content.forEach(item => {
     item.style.display = 'none';
   });
   tab.forEach(item => {
-    item.classList.remove('blank-active');
+    item.classList.remove('usa-active');
   });
 }
 
 function showTabContent(i = 0) {
   content[i].style.display = 'grid';
-  tab[i].classList.add('blank-active');
+  tab[i].classList.add('usa-active');
 }
 
 
